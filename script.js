7f0160587a8ba9c1845e7c7a910825e4d78d86c5
Lukose, Jason <jlukose@gatech.edu>
2016-02-06 07:51:06 -0500

    CSS,HTML,JS

---
+++ script.js
@@ -0,0 +1,9 @@
+ mapboxgl.accessToken = 'pk.eyJ1Ijoiamx1a29zZTEyIiwiYSI6ImNpa2F0N2dvZDBsejV1eGt1bXRwcjMxdWsifQ.lCB8nj4nEEf2vmiXcWkqSw';
+
+
+        var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+        center: [-74.50, 40], // starting position
+        zoom: 0// starting zoom
+        });
